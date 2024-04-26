9a9b0e710e525119ab3e747d2dc71b3ae8a22e82
kiranravi_dev <kiranravi.dev@gmail.com>
2024-04-27 00:32:28 +0530

    Button Added text  Now changed

--- src/Homepage/components/Homepage.jsx
+++ src/Homepage/components/Homepage.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
 
 const Homepage = () => {
+
+const [change,setChange]=useState(false)
+const handleNameChange = ()=>{
+    setChange(!change)
+}
   return (
+    <>
     <div>Homepage</div>
+     <Button  onClick={handleNameChange}>Change the text here</Button>
+    <div>
+        {!change ? "not changed" : "changed"}
+    </div>
+    </>
   )
 }
 
